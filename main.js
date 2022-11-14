91a0adb25367681ecd7e5278a290fa78ed60ed0e
Ibrahim <ibrachar68@gmail.com>
2022-11-14 14:44:47 +0100

    fine esercizio lista

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let listaSpesa=["carne", "pesce", "frutta", "verdura"];
+let ul=document.querySelector("ul");
 // for(i=0;i<listaSpesa.length;i++){
 //     console.log(listaSpesa[i]);
 // }
@@ -10,18 +11,23 @@ let listaSpesa=["carne", "pesce", "frutta", "verdura"];
       i++;
    }
 let aggiungi=document.querySelector("button");
+for(i=0;i<listaSpesa.length;i++){
+    let li=document.createElement("li");
+    li.classList.add("list");
+    ul.append(li);
+    li.append(listaSpesa[i]);
+   }
 aggiungi.addEventListener("click",
 function(){
     let lista=document.getElementById("lista").value;
     listaSpesa.push(lista);
      console.log(listaSpesa);
-     let ul=document.querySelector("ul");
-    for(i=0;i<listaSpesa.length;i++){
      let li=document.createElement("li");
-     ul.append(li);
-     li.append(listaSpesa[i]);
-
-    }
+     li.classList.add("list");
+      ul.append(li);
+      li.append(lista);
+       
+   
 }
 
 )
